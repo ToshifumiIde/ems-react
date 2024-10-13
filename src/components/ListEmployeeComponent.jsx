82546cdad8cf759d14502b1643df949715c9e5b0
井手俊文 <toshifumiide198765@gmail.com>
2024-10-13 21:28:44 +0900

    #11 add Delete button in ListEmployeeComponent

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { listEmployees } from "../services/EmployeeService";
+import { listEmployees, deleteEmployee } from "../services/EmployeeService";
 import { useNavigate } from "react-router-dom";
 
 const ListEmployeeComponent = () => {
@@ -8,19 +8,48 @@ const ListEmployeeComponent = () => {
 
   // API側の従業員情報を同期取得する
   useEffect(() => {
+    getAllEmployees();
+  }, []);
+
+  /**
+   * REST API を用いて従業員を全件取得する
+   * setEmployees()に取得データを渡す
+   */
+  function getAllEmployees() {
     listEmployees()
       .then((response) => {
         setEmployees(response.data);
       })
       .catch((error) => console.error(error));
-  }, []);
+  }
 
+  /**
+   * 新規従業員を生成する
+   */
   function addNewEmployee() {
     navigator("/add-employee");
   }
 
+  /**
+   * 指定したuuidの従業員を編集する
+   * @param uuid
+   */
   function editEmployee(uuid) {
     navigator(`/edit-employee/${uuid}`);
+  }
+
+  /**
+   * 指定したuuidの従業員を削除する
+   * 従業員の削除に成功した場合、再度全従業員を取得する
+   * @param uuid
+   */
+  function removeEmployee(uuid) {
+    console.log(uuid);
+    deleteEmployee(uuid)
+      .then((response) => {
+        getAllEmployees();
+      })
+      .catch((error) => console.error(error));
   }
 
   return (
@@ -31,11 +60,13 @@ const ListEmployeeComponent = () => {
       </button>
       <table className="table table-striped table-bordered">
         <thead>
-          <th>Employee Uuid</th>
-          <th>Employee First Name</th>
-          <th>Employee Last Name</th>
-          <th>Employee Email Id</th>
-          <th>Action</th>
+          <tr>
+            <th>Employee Uuid</th>
+            <th>Employee First Name</th>
+            <th>Employee Last Name</th>
+            <th>Employee Email Id</th>
+            <th>Action</th>
+          </tr>
         </thead>
         <tbody>
           {employees.map((employee) => (
@@ -50,6 +81,12 @@ const ListEmployeeComponent = () => {
                   onClick={() => editEmployee(employee.uuid)}
                 >
                   Update
+                </button>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => removeEmployee(employee.uuid)}
+                >
+                  Delete
                 </button>
               </td>
             </tr>
