f5db512c7c5e7b1e08cd0d4e0599de12ce69a898
井手俊文 <toshifumiide198765@gmail.com>
2024-10-13 12:05:56 +0900

    #5 setting router

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,21 @@ import HelloWorld from "./HelloWorld";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <HeaderComponent />
-      <HelloWorld />
-      <ListEmployeeComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          {/* http://localhost:4000 */}
+          <Route path="/" element={<ListEmployeeComponent />}></Route>
+          {/* http://localhost:4000/employees */}
+          <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   );
 }
