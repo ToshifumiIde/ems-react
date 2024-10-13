ca3b5a34ac6259e7b76680f7afe16fde167073c5
井手俊文 <toshifumiide198765@gmail.com>
2024-10-13 14:31:52 +0900

    #6 add createEmployee method in EmployeeService

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const REST_API_BASE_URL = "http://localhost:4000/api/employees";
 
-const listEmployees = () => axios.get(REST_API_BASE_URL);
+export const listEmployees = () => axios.get(REST_API_BASE_URL);
 
-export default listEmployees;
+export const createEmployee = (employee) => axios.post(REST_API_BASE_URL, employee);
