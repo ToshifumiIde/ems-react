fa1f26100874ddb58381f8c202bf3ff7885770a1
井手俊文 <toshifumiide198765@gmail.com>
2024-10-19 09:33:49 +0900

    #12 add navbar in Header

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const HeaderComponent = () => {
   return (
     <div>
       <header>
-        <nav className="navbar navbar-dark bg-dark">
-          <a className="navbar-brand" href="https://www.javaguides.net">Employee Management System</a>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="https://www.javaguides.net">
+              Employee Management System
+            </a>
+            <div className="collapse d-inline navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/employees">
+                    Employees
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/departments">
+                    Departments
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
         </nav>
       </header>
     </div>
