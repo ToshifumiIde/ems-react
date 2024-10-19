2faebe994c6323c9d9d6d711b1c39201070d0bee
井手俊文 <toshifumiide198765@gmail.com>
2024-10-19 12:03:16 +0900

    #15 add updateDepartment method in DepartmentService.js

--- src/services/DepartmentService.js
+++ src/services/DepartmentService.js
@@ -3,4 +3,9 @@ import axios from "axios";
 const DEPARTMENT_REST_API_BASE_URL = "http://localhost:4000/api/departments";
 
 export const listAllDepartments = () => axios.get(DEPARTMENT_REST_API_BASE_URL);
+
 export const createDepartment = (department) => axios.post(DEPARTMENT_REST_API_BASE_URL, department);
+
+export const getDepartmentByUuid = (uuid) => axios.get(DEPARTMENT_REST_API_BASE_URL + "/" + uuid);
+
+export const updateDepartmentByUuid = (uuid, department) => axios.put(DEPARTMENT_REST_API_BASE_URL + "/" + uuid , department);
